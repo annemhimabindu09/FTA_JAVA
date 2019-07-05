7b10919037db07749f9616921cef14c06ab192ae
Hima <Hima_Annem@epam.com>
2019-07-05 12:10:13 +0530

    First code

---
+++ src/main/java/com/fta/code1/Firstclass/javabasics/FirstJavaclass.java
@@ -0,0 +1,10 @@
+package com.fta.code1.Firstclass.javabasics;
+
+public class FirstJavaclass{
+    public static void main(String[] args) {
+        System.out.println("**********");
+        System.out.println("Hello java");
+        System.out.println("*********");
+
+    }
+}
